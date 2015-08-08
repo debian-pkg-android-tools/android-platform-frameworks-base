9e55f7f0dd2d1f62afcc6fa4b6287332009c096a
Kai-Chung Yan <seamlikok@gmail.com>
2015-08-08 20:11:49 +0800

    Use makefiles to generate symlinks

--- debian/libandroidfw.mk
+++ debian/libandroidfw.mk
@@ -18,11 +18,13 @@ LDFLAGS += -fPIC -shared -Wl,-soname,$(NAME).so.5 -lz \
            -L/usr/lib/android -lziparchive -lutils -lcutils -llog
 
 build: $(OBJECTS)
-	c++ $^ -o $(NAME).so $(LDFLAGS)
+	c++ $^ -o $(NAME).so.${UPSTREAM_LIBVERSION} $(LDFLAGS)
 	ar rs $(NAME).a $^
+	ln -s $(NAME).so.${UPSTREAM_LIBVERSION} $(NAME).so
+	ln -s $(NAME).so.${UPSTREAM_LIBVERSION} $(NAME).so.5
 
 clean:
-	rm -f *.so *.a *.o
+	rm -f *.so* *.a *.o
 
 $(OBJECTS): %.o: %.cpp
 	c++ $< -o $@ $(CXXFLAGS) $(CPPFLAGS)
