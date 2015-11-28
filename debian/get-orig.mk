e29dcaed23d0a9a8a6d3fddf1838770a1c515b51
Kai-Chung Yan <seamlikok@gmail.com>
2015-11-28 17:00:25 +0800

    Bump to android-6.0.0_r26 and use cleaner approach for get-orig-source

--- debian/get-orig.mk
+++
@@ -1,5 +0,0 @@
-build:
-	wget https://android.googlesource.com/platform/frameworks/base/+archive/$(UPSTREAM_TAG).tar.gz
-	mkdir $(PACKAGE_NAME)-$(UPSTREAM_VERSION)
-	tar -f $(UPSTREAM_TAG).tar.gz -z -x --directory $(PACKAGE_NAME)-$(UPSTREAM_VERSION)
-	tar --create --xz -X ../debian/orig.excludes -f $(PACKAGE_NAME)_$(UPSTREAM_VERSION).orig.tar.xz $(PACKAGE_NAME)-$(DEBIAN_VERSION)
