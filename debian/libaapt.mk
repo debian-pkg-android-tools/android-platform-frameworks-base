dc97525db789e7f05f266ef4538166c10ec0c614
Kai-Chung Yan <seamlikok@gmail.com>
2016-01-10 21:45:40 +0800

    d/libaapt.mk: Remove version suffix

--- debian/libaapt.mk
+++ debian/libaapt.mk
@@ -30,9 +30,8 @@ LDFLAGS += -shared -Wl,-soname,$(NAME).so.0 -Wl,-rpath=/usr/lib/android \
            -lpng -lexpat -lz -L/usr/lib/android -llog -lutils -L. -landroidfw
 
 build: $(SOURCES)
-	$(CXX) $^ -o $(NAME).so.$(ANDROID_LIBVERSION) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
-	ln -s $(NAME).so.$(ANDROID_LIBVERSION) $(NAME).so
-	ln -s $(NAME).so.$(ANDROID_LIBVERSION) $(NAME).so.0
+	$(CXX) $^ -o $(NAME).so.0 $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
+	ln -s $(NAME).so.0 $(NAME).so
 
 clean:
 	$(RM) $(NAME).so*
